869c2d97eb3df7e6cf0c35dcd2c7240c6723e95c
zaidalial <zaid71288@gmail.com>
2024-11-29 02:05:13 +0300

    init

---
+++ firstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FirstClassLibrary;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("test");
+var person = new Person
+{
+  Name = "farouq",
+  Id = 0,
+  Description = "mcxvn",
+};
+Console.WriteLine(person.Name);
+Console.WriteLine(person.Description);
